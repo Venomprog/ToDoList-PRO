ff74e182affd6a87af836422eb06c06c2838a132
Gleb <glebshaevvv@mail.ru>
2022-12-08 22:57:48 +0300

    comments

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ function render(){
     let html = ``;
     todos.forEach(todo =>{
         if (todo.done){
-            return;   //Если задача выполнена, то она не выводится!!
+            return;   //Если задача выполнена, то она не выводится!!!
         }
 
         html+= `
